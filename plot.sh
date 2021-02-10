d63d63cbb76b004ff0cebe4175fddedecf2ae4e7
Juan Cruz Viotti <jv@jviotti.com>
2021-02-09 23:18:02 -0400

    Add plot title
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- plot.sh
+++ plot.sh
@@ -9,8 +9,9 @@ for document in results/*
 do
   echo ">> Plot $document"
   DOCUMENT_ID="$(basename "$document")"
+  DOCUMENT_NAME="$(cat "benchmark/$DOCUMENT_ID/NAME" | tr -d '\n')"
   cd "$document"
-  gnuplot "$GNUPLOT_FILE"
+  gnuplot -e "description=\"$DOCUMENT_NAME\"" "$GNUPLOT_FILE"
   cd "$ROOT_DIRECTORY"
   mv "$document/chart.png" "$ROOT_DIRECTORY/charts/$DOCUMENT_ID.png"
 done
