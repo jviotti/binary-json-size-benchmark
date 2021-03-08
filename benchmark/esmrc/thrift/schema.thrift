dc15777accb9f17c904b4092a8739923f3dd8ecc
Juan Cruz Viotti <jv@jviotti.com>
2021-03-08 14:31:55 -0400

    Add esmrc
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ benchmark/esmrc/thrift/schema.thrift
@@ -0,0 +1,8 @@
+struct Main {
+  1: required bool cjs,
+  2: required list<string> mainFields,
+  3: required string mode,
+  4: required bool force,
+  5: required bool cache,
+  6: required bool sourceMap
+}
