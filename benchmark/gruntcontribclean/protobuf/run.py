1d724a990fe627b489a43152a0ed276f69141ca0
Juan Cruz Viotti <jv@jviotti.com>
2021-03-11 11:20:32 -0400

    Add gruntcontribclean
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/gruntcontribclean/protobuf/run.py
+++ benchmark/gruntcontribclean/protobuf/run.py
@@ -1,7 +1,20 @@
 def encode(json, schema):
     payload = schema.Main()
+    payload.foo.extend(json['foo'])
+    payload.main.src.extend(json['main']['src'])
+    payload.options.force = json['options']['force']
+    payload.options.noWrite = json['options']['no-write']
     return payload
 
 def decode(payload):
     return {
+        'foo': payload.foo._values,
+        'main': {
+            'files': dict(zip(payload.main.files.ListFields(), [])),
+            'src': payload.main.src._values
+        },
+        'options': {
+            'force': payload.options.force,
+            'no-write': payload.options.noWrite
+        }
     }
