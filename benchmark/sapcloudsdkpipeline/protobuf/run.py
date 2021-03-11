451faf42dd5bf663bcec81008a132504beaa12a5
Juan Cruz Viotti <jv@jviotti.com>
2021-03-11 14:34:00 -0400

    Add sapcloudsdkpipeline
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/sapcloudsdkpipeline/protobuf/run.py
+++ benchmark/sapcloudsdkpipeline/protobuf/run.py
@@ -1,7 +1,13 @@
 def encode(json, schema):
     payload = schema.Main()
+    payload.general = json['general'] or 0
+    payload.stages = json['stages'] or 0
+    payload.steps = json['steps'] or 0
     return payload
 
 def decode(payload):
     return {
+        'general': payload.general or None,
+        'stages': payload.general or None,
+        'steps': payload.general or None
     }
