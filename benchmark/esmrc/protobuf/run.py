6a47ae430653577f76592ad4c659d9474dc01c18
Juan Cruz Viotti <jv@jviotti.com>
2021-03-11 10:50:01 -0400

    Add esmrc
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/esmrc/protobuf/run.py
+++ benchmark/esmrc/protobuf/run.py
@@ -1,7 +1,19 @@
 def encode(json, schema):
     payload = schema.Main()
+    payload.cjs = json['cjs']
+    payload.mainFields.extend(json['mainFields'])
+    payload.mode = json['mode']
+    payload.force = json['force']
+    payload.cache = json['cache']
+    payload.sourceMap = json['sourceMap']
     return payload
 
 def decode(payload):
     return {
+      'cjs': payload.cjs,
+      'mainFields': payload.mainFields._values,
+      'mode': payload.mode,
+      'force': payload.force,
+      'cache': payload.cache,
+      'sourceMap': payload.sourceMap
     }
