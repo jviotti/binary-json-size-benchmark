394db4f42445a348347fe01c7d01f85043ec9f3d
Juan Cruz Viotti <jv@jviotti.com>
2021-03-11 11:28:51 -0400

    Add jsonesort
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/jsonesort/protobuf/run.py
+++ benchmark/jsonesort/protobuf/run.py
@@ -1,7 +1,12 @@
 def encode(json, schema):
     payload = schema.Main()
+    payload.sort.extend(json['$sort'])
+    payload.byX = json['by(x)']
     return payload
 
 def decode(payload):
     return {
+        '$sort': payload.sort._values,
+        'by(x)': payload.byX
     }
+
