c369e4f631a1a363c481d17113ef1d023fdb34f9
Juan Cruz Viotti <jv@jviotti.com>
2021-03-11 11:24:33 -0400

    Add imageoptimizerwebjob
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/imageoptimizerwebjob/protobuf/run.py
+++ benchmark/imageoptimizerwebjob/protobuf/run.py
@@ -1,7 +1,18 @@
 def encode(json, schema):
     payload = schema.Main()
+    optimization1 = payload.optimizations.add()
+    optimization1.includes.extend(json['optimizations'][0]['includes'])
+    optimization1.excludes.extend(json['optimizations'][0]['excludes'])
+    optimization1.lossy = json['optimizations'][0]['lossy']
     return payload
 
 def decode(payload):
     return {
+        'optimizations': [
+            {
+              'includes': payload.optimizations[0].includes._values,
+              'excludes': payload.optimizations[0].excludes._values,
+              'lossy': payload.optimizations[0].lossy
+            }
+        ]
     }
