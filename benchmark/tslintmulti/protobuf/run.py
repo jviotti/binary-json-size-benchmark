5db81762238d294b9c25f75560b5b7b4cb9587a4
Juan Cruz Viotti <jv@jviotti.com>
2021-03-11 14:38:52 -0400

    Add tslintmulti
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/tslintmulti/protobuf/run.py
+++ benchmark/tslintmulti/protobuf/run.py
@@ -1,7 +1,20 @@
 def encode(json, schema):
     payload = schema.Main()
+    payload.rules.noAny.extend(json['rules']['no-any'])
+    payload.rules.radix.extend(json['rules']['radix'])
+    payload.rules.orderedImports.options.groupedImports = \
+        json['rules']['ordered-imports']['options']['grouped-imports']
     return payload
 
 def decode(payload):
     return {
+      'rules': {
+        'no-any': payload.rules.noAny._values,
+        'radix': payload.rules.radix._values,
+        'ordered-imports': {
+          'options': {
+            'grouped-imports': payload.rules.orderedImports.options.groupedImports
+          }
+        }
+      }
     }
