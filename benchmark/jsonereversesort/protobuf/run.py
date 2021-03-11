b0b7ea160765c66f86afefbca6030afe65a3e6b8
Juan Cruz Viotti <jv@jviotti.com>
2021-03-11 11:27:25 -0400

    Add jsonereversesort
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/jsonereversesort/protobuf/run.py
+++ benchmark/jsonereversesort/protobuf/run.py
@@ -1,7 +1,19 @@
 def encode(json, schema):
     payload = schema.Main()
+    payload.let.x.extend(json['$let']['x'])
+    payload.in1.reverse.sort.eval = json['in']['$reverse']['$sort']['$eval']
+    payload.in1.reverse.byX = json['in']['$reverse']['by(x)']
     return payload
 
 def decode(payload):
     return {
+      '$let': {
+        'x': payload.let.x._values
+      },
+      'in': {
+        '$reverse': {
+          '$sort': { '$eval': payload.in1.reverse.sort.eval },
+          'by(x)': payload.in1.reverse.byX
+        }
+      }
     }
