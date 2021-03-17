9270455631cb36611f763a7b1467f2aac53d499f
Juan Cruz Viotti <jv@jviotti.com>
2021-03-17 19:39:09 -0400

    Delete geojsonredundant
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/geojsonredundant/thrift/run.py
+++
@@ -1,8 +0,0 @@
-def encode(json, schema):
-    payload = schema.Main()
-    payload.type = json['type']
-    payload.coordinates = json['coordinates']
-    return payload
-
-def decode(payload):
-    return payload.__dict__
